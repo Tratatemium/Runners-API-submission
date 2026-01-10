6ccb067e029b8c8d5b18b9b1afe411c219a891d8
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-01-10 20:49:36 +0100

    feat: implement initial API with runtime endpoint and dotenv configuration

---
+++ api.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { getRunsCollection } = require("./database.js");
+
+const PORT = process.env.PORT;
+
+const serverTimeStart = Date.now();
+
+app.get('/', (req, res) => {
+    res.sendStatus(501);
+});
+
+app.get('/runtime', (req, res) => {
+    const serverTimeCurrent = (Date.now() - serverTimeStart) / 1000;
+    const serverTimeCurrentRounded = Math.round(serverTimeCurrent * 10) / 10;
+    res.send(`Server is running for ${serverTimeCurrentRounded} s.`);
+});
+
+
+/* ================================================================================================= */
+/*  LISTEN                                                                                           */
+/* ================================================================================================= */
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+});
