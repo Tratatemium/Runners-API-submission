fbd65963151e7d15f27495a11b73259103bce677
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-01-10 21:22:59 +0100

    style: format code for consistency in API routes

--- api.js
+++ api.js
@@ -10,22 +10,22 @@ const PORT = process.env.PORT;
 
 const serverTimeStart = Date.now();
 
-app.get('/', (req, res) => {
-    res.sendStatus(501);
+app.get("/", (req, res) => {
+  res.sendStatus(501);
 });
 
-app.get('/runtime', (req, res) => {
-    const serverTimeCurrent = (Date.now() - serverTimeStart) / 1000;
-    const serverTimeCurrentRounded = Math.round(serverTimeCurrent * 10) / 10;
-    res.send(`Server is running for ${serverTimeCurrentRounded} s.`);
+app.get("/runtime", (req, res) => {
+  const serverTimeCurrent = (Date.now() - serverTimeStart) / 1000;
+  const serverTimeCurrentRounded = Math.round(serverTimeCurrent * 10) / 10;
+  res.send(`Server is running for ${serverTimeCurrentRounded} s.`);
 });
 
+// app.get();
 
 /* ================================================================================================= */
 /*  LISTEN                                                                                           */
 /* ================================================================================================= */
 
-
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
+  console.log(`Listening on port ${PORT}...`);
 });
